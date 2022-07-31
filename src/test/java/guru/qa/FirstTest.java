4b8234c12ab241d9b2d6d272767866cea6626faa
YuliaOrl <orbitalik32@gmail.com>
2022-07-31 17:33:16 +0300

    First commit

---
+++ src/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,14 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void firstTest () {
+        Assertions.assertTrue(3 > 2);
+    }
+
+}
